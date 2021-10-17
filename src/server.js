d28a5d501ccbd8b44db79a512a440e5771ee2f01
jinoo7099 <vxc7099@gmail.com>
2021-10-17 13:54:48 +0900

    Nicknames part Two

---
+++ src/server.js
@@ -0,0 +1,38 @@
+import express from "express"
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log("hello");
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anon";
+    console.log("Connected to Browser");
+    socket.on("close", () => console.log("Disconnected from the Browser"));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => {
+                    aSocket.send(`${socket.nickname}: ${message.payload}`)
+                });
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
+    })
+});
+
+server.listen(8000, handleListen);
