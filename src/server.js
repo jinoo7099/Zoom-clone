b86381e9865b2ac52d6b276d7f69d95e731eed7b
jinoo7099 <vxc7099@gmail.com>
2021-10-20 02:30:44 +0900

    Add Call Rooms

--- src/server.js
+++ src/server.js
@@ -16,5 +16,13 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 
+wsServer.on("connection", (socket) => {
+    socket.on("join_room", (roomName, done) => {
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit("welcome");
+    })
+})
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(8000, handleListen);
